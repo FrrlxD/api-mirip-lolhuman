5a51300bccfa5d92737f7cc3ec96a6e1503ba5e3
FrrlxD <105418953+FrrlxD@users.noreply.github.com>
2023-01-10 14:38:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'FrrlDfniX7' 
+global.apikey = ["FrrlDfniX7", "FrrlDfniBotz", "FrrlDfni"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
